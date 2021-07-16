93f8c14365d0a0bab32f75e38457f37b550ae8b2
davood <davoodkhany@gmail.com>
2021-07-16 16:36:25 +0330

    Rellashn Ship Rule & Permossion & User

---
+++ app/Permission.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    protected $fillable = ['name' , 'label'];
+
+    public function rules(){
+        return $this->belongsToMany(Rule::class);
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+    
+}
