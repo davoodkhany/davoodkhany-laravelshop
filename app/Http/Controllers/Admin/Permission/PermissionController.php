8ee49efeced13066965199d3bf70164aaca91e04
davood <davoodkhany@gmail.com>
2021-07-24 22:53:37 +0330

    Permission and rule And User Added

---
+++ app/Http/Controllers/Admin/Permission/PermissionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Permission;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+}
+
--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,9 @@ return view('admin.index');
 
 Route::resource('users','UserController');
 
+Route::get('users/{$user}/permission', 'Permission/PermissionController@create')->name('admin.users.permission');
+Route::post('users/{$user}/permission', 'Permission/PermissionController@store')->name('admin.users.permission.store');
+
 Route::resource('permission', 'PermissionController');
 Route::resource('rule', 'RuleController');
 
