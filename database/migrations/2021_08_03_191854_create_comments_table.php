fe3536c61b9ae5e10215f23e065bd81c1956b21e
davood <davoodkhany@gmail.com>
2021-08-03 23:00:24 +0330

    polimorth

--- database/migrations/2021_08_03_191854_create_comments_table.php
+++ database/migrations/2021_08_03_191854_create_comments_table.php
@@ -15,6 +15,18 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+
+            $table->unsignedBigInteger('commentable_id');
+
+        $table->string('commentable_type');
+        $table->unsignedInteger('parent_id')->default(0);
+
+        $table->boolean('approved')->default(0);
+        $table->text('comment');
             $table->timestamps();
         });
     }
